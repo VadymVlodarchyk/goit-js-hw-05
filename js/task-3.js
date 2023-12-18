b9e6c5725cb97c0608674f82c6a7da6051f718c4
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2023-12-18 13:07:26 +0000

    bnbm

--- js/task-3.js
+++ js/task-3.js
@@ -130,4 +130,4 @@ const allUsers = [
 
 console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(allUsers, "female")); // 8863 ()
