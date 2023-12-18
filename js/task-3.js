60f3b3b4dc8eeec1d256ec8f2a141d004b6d16d3
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2023-12-18 12:48:48 +0000

    diiif

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,83 @@
-function checkForSpam(message) {
-    const banWord1 = 'spam';
-    const banWord2 = 'sale';
-    const hasSpam1 = message.toLowerCase().includes(banWord1);
-    const hasSpam2 = message.toLowerCase().includes(banWord2);
-    const result = hasSpam1 || hasSpam2;
-    return result;
+const sortByDescendingFriendCount = (users) => {
+    const sortFriends = users
+        .toSorted((a, b) => b.friends.length - a.friends.length);
+    return sortFriends;
 }
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+console.log(
+  sortByDescendingFriendCount([
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"],
+      gender: "male"
+    },
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"],
+      gender: "female"
+    },
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      gender: "male"
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      gender: "female"
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      gender: "male"
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      gender: "male"
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      gender: "female"
+    }
+  ])
+);
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
--- js/task-4.js
+++ js/task-4.js
@@ -1,34 +1,49 @@
-function getShippingCost(country) {
-    let price;
-    let message;
-    switch (country) {
-        case "China":
-            price = 100;
-            message = `Shipping to ${country} will cost ${price} credits`;
-            break;
-        
-        case "Chile":
-            price = 250;
-            message = `Shipping to ${country} will cost ${price} credits`;
-            break;
-        
-        case "Australia":
-            price = 170;
-            message = `Shipping to ${country} will cost ${price} credits`;
-            break;
-        
-        case "Jamaica":
-            price = 120;
-            message = `Shipping to ${country} will cost ${price} credits`;
-            break;
-        
-        default: message = "Sorry, there is no delivery to your country"     
-    }
-    return message;
-}
-console.log(getShippingCost("Australia"));
-console.log(getShippingCost("Germany"));
-console.log(getShippingCost("China")); 
-console.log(getShippingCost("Chile")); 
-console.log(getShippingCost("Jamaica")); 
-console.log(getShippingCost("Sweden"));
+const getTotalBalanceByGender = (users, gender) => {
+    const totalBalance = users
+        .filter(user => user.gender === gender)
+        .reduce((acc, item) => acc + item.balance, 0);
+    return totalBalance;
+};
+
+
+const allUsers = [
+    {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+
+console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
