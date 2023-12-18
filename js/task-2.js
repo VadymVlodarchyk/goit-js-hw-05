9cb7e06d0173a8e9a17e28a999c043025cf2bb70
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2023-12-18 23:06:34 +0000

    vbn

--- js/task-2.js
+++ js/task-2.js
@@ -4,38 +4,38 @@ const getUsersWithFriend = (users, friendName) => {
 };
 
 
-const allUsers = [
-    {
-        name: "Moore Hensley",
-        friends: ["Sharron Pace"]
-    },
-    {
-        name: "Sharlene Bush",
-        friends: ["Briana Decker", "Sharron Pace"]
-    },
-    {
-        name: "Ross Vazquez",
-        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
-    },
-    {
-        name: "Elma Head",
-        friends: ["Goldie Gentry", "Aisha Tran"]
-    },
-    {
-        name: "Carey Barr",
-        friends: ["Jordan Sampson", "Eddie Strong"]
-    },
-    {
-        name: "Blackburn Dotson",
-        friends: ["Jacklyn Lucas", "Linda Chapman"]
-    },
-    {
-        name: "Sheree Anthony",
-        friends: ["Goldie Gentry", "Briana Decker"]
-    }
+const allUserss = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"]
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"]
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"]
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"]
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"]
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"]
+  }
 ];
 
-console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+console.log(getUsersWithFriend(allUserss, "Briana Decker")); 
 // [
 //   {
 //     name: "Sharlene Bush",
@@ -47,7 +47,7 @@ console.log(getUsersWithFriend(allUsers, "Briana Decker"));
 //   }
 // ]
 
-console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+console.log(getUsersWithFriend(allUserss, "Goldie Gentry"));
 // [
 //   {
 //     name: "Elma Head",
@@ -59,4 +59,4 @@ console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
 //   }
 // ]
 
-console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+console.log(getUsersWithFriend(allUserss, "Adrian Cross" )); // []
